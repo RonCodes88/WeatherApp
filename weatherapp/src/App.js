c1ad6a0512b95215b0fb7bcdb7f20f057eac705a
Ronald Li <137369375+RonCodes88@users.noreply.github.com>
2024-02-03 02:02:15 -0800

    Update App.js

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState } from "react";
 
 const api = {
-  key: "4dee4a69b8f2a6628610f505969671fc",
+  key: "xxxxxxxxxxxx",
   base: "http://api.openweathermap.org/data/2.5/",
 };
 
